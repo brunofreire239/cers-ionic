b24fcf9b7d184c28638c033cb938118a229505ac
bruno.silva <bruno.silva@mv.com.br>
2017-07-14 11:46:29 -0300

    ajustes finais

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { NavController , ToastController} from 'ionic-angular';
 
 import { MainPage } from '../../pages/pages';
 import { CardsPage }  from '..//cards/cards';
 
 import { ContentPage } from '../content/content';
-import { Platform } from 'ionic-angular';
+import { Platform,Content } from 'ionic-angular';
 /**
  * The Welcome Page is a splash page that quickly describes the app,
  * and then directs the user to create an account or log in.
@@ -17,7 +17,7 @@ import { Platform } from 'ionic-angular';
   templateUrl: 'welcome.html'
 })
 export class WelcomePage {
-
+ @ViewChild(Content) cont: Content;
     email: string;
     password: string;
   constructor(public toastCtrl: ToastController,
@@ -35,6 +35,41 @@ export class WelcomePage {
         });
       toast.present();
    
+  }
+
+  /**
+   * Reponsavel por da o Scroll na tela ate o determinado elemento.
+   */
+  scrollTo() {
+    console.log("--------------------------------------------------------------------------------");
+    let elemento = document.getElementById("testeScroll");
+    console.log("CODIGO =  " + elemento)
+    if (elemento) {
+
+      let posicaoElemento = this.getPosition(elemento);
+      this.cont.scrollToTop(0).then(scroll => {
+        this.cont.scrollTo(0, posicaoElemento, 1000).then(data => {
+
+          
+        })
+      })
+
+    }
+
+  }
+
+  /**
+   * Pega a posição do horario na tela .
+   */
+  getPosition(element) {
+
+    var yPosition = 0;
+
+    while (yPosition == 0) {
+      yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+      element = element.offsetParent;
+    }
+    return yPosition;
   }
 
 }
